51b22060f65e2d4523686755a534f0d19a69f7d2
arshinmar <arsh.kadakia18@gmail.com>
2020-01-12 14:25:16 -0500

    Update main3.py

--- main3.py
+++ main3.py
@@ -7,16 +7,15 @@ if __name__ == "__main__":
     #get file path
     command_line_args = sys.argv
     filename = command_line_args[1] #Command_line_input: python main3.py filename
-
     original_list,desired_list = parse(filename)
     temp = makehash(original_list, desired_list)
     print(temp)
     ht = temp[0]
     new_original_list = temp[1]
     new_desired_list = temp[2]
-    final = compute_DP_matrix(new_original_list, new_desired_list)
-    print(final)
-    final2 = process_DP_matrix(new_original_list,new_desired_list,final)
+    dp = compute_DP_matrix(new_original_list, new_desired_list)
+    print(dp)
+    final2 = process_DP_matrix(new_original_list,new_desired_list,dp)
     print(final2)
     #best = get_best_path(final2)
     best2 = generate2(final2[0],ht)
