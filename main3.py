557f78f5a0a86356ee10b8b7074b13979716ac11
arshinmar <arsh.kadakia18@gmail.com>
2020-01-12 20:57:28 -0500

    Final (most likely) upload
    Updated, notation and printing is now complete, not including sorting notation for the provided actions (check parallel problem)

--- main3.py
+++ main3.py
@@ -9,19 +9,19 @@ if __name__ == "__main__":
     filename = command_line_args[1] #Command_line_input: python main3.py filename
     original_list,desired_list = parse(filename)
     temp = makehash(original_list, desired_list)
-    print('blah',temp)
+    #print('blah',temp)
     ht = temp[0]
     new_original_list = temp[1]
     new_desired_list = temp[2]
     dp = compute_DP_matrix(new_original_list, new_desired_list)
-    print('blah2',dp)
+    #print('blah2',dp)
     actions = process_DP_matrix(new_original_list,new_desired_list,dp)
-    print('blah3',actions)
+    #print('blah3',actions)
     #print(print_best_path(actions))
     #best = get_best_path(final2)
     best2 = generate2(actions,ht)
     # write to file
-    print('a',best2)
+    #print('a',best2)
     with open(filename[0:len(filename)-3] + ".out" , "w") as output_file:
         for line in best2:
             output_file.write(line + "\n")
--- part3.py
+++ part3.py
@@ -51,7 +51,6 @@ def makehash(oa, da):
             new_da += [ht_idx] #add to the new array
             ht_idx += 1 #increment ht index
         exists = False
-    print('Hash',ht)
     return [ht, new_oa, new_da]
 #-------------------------------------------------------------------------------
 def compute_DP_matrix(s1, s2):
@@ -84,7 +83,6 @@ def compute_DP_matrix(s1, s2):
                 substitutionCost = 1
 
             dp[i][j] = min(dp[i-1][j] + 1, dp[i][j-1] + 1, dp[i-1][j-1] + substitutionCost)
-    print('DP matrix',dp)
     return dp
 
 '''def process_DP_matrix(s1, s2, dp):
@@ -185,7 +183,6 @@ def get_best_path(all_paths):
             if cost<best:
                 best=cost
                 best_path_id=i
-    print('Best path',all_paths)
     return all_paths
 
 def print_best_path(path_segments):
@@ -198,9 +195,13 @@ def print_best_path(path_segments):
 
     for seg in path_segments:
         books = ""
-        for book in seg[3]:
-            books += '"%s"' %(str(book)) + ' '
-        books = books[0:-1]
+        for i in range(3,len(seg),1):
+            #books += '"%s"' %(str(book)) + ' '
+            a=str('"')
+            b=str('"')
+            main=a+seg[i]+b
+            books+=main
+        #books = books[0:-1]
 
         if seg[0] == INSERTION_ID:
             best_path.insert(0, 'Insert %d-%d, %s' % (seg[1], seg[2], books) )
@@ -210,14 +211,13 @@ def print_best_path(path_segments):
 
         elif seg[0] == REPLACEMENT_ID:
             best_path.insert(0, 'Replace %d-%d, %s' % (seg[1], seg[2], books) )
-    print('Printed best path',best_path)
     return best_path
 
 def generate2(main_path,ht):
     # remember to call generate with a list of actions like below
     #actions = ['Insert​ ​0-0​, ​"Religion and Mythology, Neil Potts"','Delete​ ​2-3','Insert​ ​3-4​, ​"Foundation, Isaac Asimov"​ ​"Intro to Algorithms, Thomas Cormen"']
     # Format of Path1: [[ActionID,Index,Character],[...],[...]]
-    previous_action=0
+    '''previous_action=0
     final=[]
     temp=[]
     start_index=0
@@ -237,12 +237,45 @@ def generate2(main_path,ht):
                 temp=[previous_action,start_index,end_index-1]
                 for i in range(start_index,end_index,1):
                     temp+=[main_path[i][2]]
-                final+=[temp]
-
+                final+=[temp]'''
+    #Commented out version above takes in a different kind of array.
+    #The following code is if one path is given:
+    #Format of Input Here: [[ActionID,Index,Character],[...],[...]]
+    previous_action=0
+    final=[]
+    temp=[]
+    start_index=0
+    end_index=0
+    previous_action=main_path[0][0]
+    quick_counter=0 #Replacing length counter
+    for i in main_path:
+        #Check if this action equals previous action
+        if i[0]!=previous_action:
+            #If not, create an entry that includes the previous_action, start_index, end_index
+            temp=[previous_action,start_index,end_index-1]
+            #Add items corresponding to all indexes between start and end indexes
+                #This does rely on assuming inserts correspond to consecutive indices.
+            for j in range(start_index,end_index,1):
+                temp+=[main_path[j][2]]
+            #Add this to the final path list.
+            final+=[temp]
+            #Start index is now current index
+            #End index is next index temporarily.
+            #Previous action is now current action.
+            start_index=i[1]
+            end_index=start_index+1
+            previous_action=i[0]
+        else:
+            end_index+=1
+        if quick_counter==len(main_path)-1: #Need Replacement due to for loop kind.
+            temp=[previous_action,start_index,end_index-1]
+            for i in range(start_index,end_index,1):
+                temp+=[main_path[i][2]]
+            final+=[temp]
+        quick_counter+=1
     #Convert back to book names
     for i in range(0,len(final),1):
         for idx in range(3, len(final[i]),1):
             num = final[i][idx]
             final[i][idx] = ht[num][1]
-    print('Printed best path generate2',final)
     return print_best_path(final)
